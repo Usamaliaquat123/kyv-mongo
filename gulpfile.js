888a8002d4186844fffba0178e585bb440dc04f5
Usamaliaquat123 <inconnent4@gmail.com>
2020-02-10 15:01:51 -0800

    add get method

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,9 @@ const  gulp = require('gulp')
 const nodemon = require('gulp-nodemon')
 
 
+gulp.task('default',() => {
+    nodemon({
+        script : "src/index.js",
+        ignore : ['node_modules']
+    })
+})
--- src/index.js
+++ src/index.js
@@ -1,4 +1,61 @@
-const mongo = process.env.MONGO_URL
+const mongo = require('mongojs')
+const EventEmitter = require('events')
+const pify = require('pify')
+class kyvMongo extends EventEmitter { 
+    constructor(url, opts){
+        super()
+
+        this.ttlSupport = false
+
+        url = url || {}
+        if(typeof url === 'string'){
+            url = {url}
+        }
+
+        if(url.uri){
+            url = Object.assign({url : url.uri}, url)
+        }
+        this.opts = Object.assign({
+            url : 'mongodb://127.0.0.1:27017',
+            collection : 'kyv'
+        })
+
+        this.db = mongo(this.opts.url)
 
 
-console.log(mongo)
+        const collection = this.db.collection(this.opts.collection)
+
+
+
+        collection.createIndex({ key: 1 },{
+            unique : true,
+            background : true
+        })
+
+
+        collection.createIndex({ expiresAt: 1 }, {
+            expireAfterSeconds: 0,
+            background: true
+        })
+
+        this.mongo = ['update', 'findone', 'remove'].reduce((obj, method) => {
+            obj[method] = pify(collection[method].bind(collection))
+            return obj;
+        },{})
+
+        this.db.on('error', err => this.emit('error',err))
+    }
+
+    get(key){
+        return this.mongo.findOne({ key })
+            .then(doc => {
+                if(doc === null){
+                    return undefined
+                }
+                return doc.value
+            })
+    }
+}
+
+
+module.exports  = kyvMongo
